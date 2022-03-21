7b3a163ff6fa4908058404829cb0defa924f4afb
uswarather <78147885+uswarather@users.noreply.github.com>
2022-03-21 20:29:23 +0530

    Add files via upload

---
+++ class 97/code.py
@@ -0,0 +1,14 @@
+# a=10
+# print(a)
+# name='uswa'
+# print(name)
+from re import S
+
+
+friends=['uswa','taki','maryam']
+# print(friends)
+# print(friends[1])
+# print(len(friends))
+for f in friends: 
+    print(f) 
+
